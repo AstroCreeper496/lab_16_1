4b598d8053f5e8bb46a7be46640339daa5d1199a
AstroCreeper496 <pluccoqlu.1927@gmail.com>
2026-02-06 14:07:57 +0700

    Update lab16_1.cpp

--- lab16_1.cpp
+++ lab16_1.cpp
@@ -32,3 +32,36 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+
+void showData(double a[], int N, int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << a[i];
+        if( (i+1) % M == 0) cout << endl;
+        else cout << " ";
+    }
+}
+
+void randData(double a[],int N,int M){
+    for(int i = 0; i < N*M; i++){
+        a[i] = rand() % 101 / 100.0;
+    }
+}
+
+void findRowSum(const double u[],double n[],int o,int wn){
+    for(int i = 0; i < o*wn; i++){
+        //cout << i << "   ";
+        if (i % wn == 0) n[i / wn] = 0;
+        n[i / wn] += u[i];
+        //cout << i << " " << i / wn << " " << u[i] << " " << n[i / wn] << " " << endl;
+    }
+}
+
+void findColSum(const double u[], double n[], int o, int wn){    
+for(int i = 0; i < wn*o; i++){
+        //cout << i << "   ";
+        if (i % o == 0) n[i / o] = 0;
+        n[i / o] += u[(i * wn % (o*wn)) + i / o];
+        //cout << i << " " << (i * wn % (o*wn)) + i / o << " " << u[(i * wn % (o*wn)) + i / o] << " " << n[i / o] << " " << endl;
+    }
+}
